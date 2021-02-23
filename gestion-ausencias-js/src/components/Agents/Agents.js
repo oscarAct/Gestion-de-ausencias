e348e0daa691eab870fb14eecac7f4e15aa75820
oscarAct <45855974+oscarAct@users.noreply.github.com>
2021-02-23 06:14:55 -0600

    Confirmando cambios

--- gestion-ausencias-js/src/components/Agents/Agents.js
+++ gestion-ausencias-js/src/components/Agents/Agents.js
@@ -163,6 +163,10 @@ export default {
   computed: {},
   mounted() {},
   methods: {
+    showDownloadMenu() {
+      $("#download-menu").fadeToggle(0);
+      $("#backdrop-bg").fadeToggle(0);
+    },
     openEditIcon(value, data, cell, row, options) {
       let element = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" class="w-5 h-5 text-blue-400 mx-auto hover:text-blue-500 ml-5 editRow" v-on:click="showMessage" viewBox="0 0 24 24" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
@@ -273,6 +277,8 @@ export default {
     },
     closeFormModal() {
       $("#form-ausencia").toggleClass("fadeOut");
+      $("#download-menu").fadeOut(0);
+      $("#backdrop-bg").fadeOut(0);
 
       $("#eliminar-ausencia").toggleClass("fadeOut");
       setTimeout(() => {
