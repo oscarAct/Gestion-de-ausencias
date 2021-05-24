7ecb070e86598f629858a827f42b3c25ade2d724
oscarAct <45855974+oscarAct@users.noreply.github.com>
2021-05-24 14:18:12 -0600

    Se agregan graficos
    Se agregan graficos para el analisis de datos

--- gestion-ausencias-js/src/main.js
+++ gestion-ausencias-js/src/main.js
@@ -4,9 +4,30 @@ import router from "./router";
 import "./assets/styles/app.css";
 import VueResource from "vue-resource";
 import VueTabulator from "vue-tabulator";
+import Chartkick from "vue-chartkick";
+import Highcharts from "highcharts";
+import { Chart } from "chart.js";
+import VueHtml2Canvas from "vue-html2canvas";
 
+Vue.use(Chartkick.use(Chart));
+Vue.use(Chartkick.use(Highcharts));
+Chartkick.options = {
+  colors: [
+    "#2f7ed8",
+    "#0d233a",
+    "#8bbc21",
+    "#910000",
+    "#1aadce",
+    "#492970",
+    "#f28f43",
+    "#77a1e5",
+    "#c42525",
+    "#a6c96a",
+  ],
+};
 Vue.use(VueTabulator);
 Vue.use(VueResource);
+Vue.use(VueHtml2Canvas);
 Vue.config.productionTip = false;
 
 new Vue({
